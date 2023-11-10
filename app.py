7d0119f915927a7c8a0a0ac7d35fdbecbb07fb08
omario530 <omarescamilla229@gmail.com>
2023-11-10 14:47:49 -0600

    archivos_i

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import messagebox
 from tkinter import filedialog
+
 from manejar_tablas import borrar_tablas, crear_tablas
 from manejar_archivos import tabla_explorador, insertar_datos_explorador, tabla_explorador_asc, insertar_datos_explorador_asc
 from manejar_insertar import insertar_datos_1, insertar_datos, insertar_datos_n, codigo_html_nivel_1, codigo_html_niveles, codigo_html_nivel_n
 from manejar_vistas import crear_vista, borrar_vista
 from crear_tabla import tabla
+
 import sqlite3
 import os
 
+# exportar datos a la BD
+conn = sqlite3.connect("static/arbol_oficios.db")
+cursor = conn.cursor()
 
 # Acccion del boton etiqueta
 def habilitar_etiqueta():
@@ -30,104 +35,115 @@ def seleccionar_carpeta():
     if respuesta:
 
         if folder_path != None:
-            obtener_archivos(folder_path)
-
-            # llamar a manejar_tablas
-            #borrar_tablas()
-            #crear_tablas()
-
-            # llamar a manejar_archivos
-            #tabla_explorador()
-            #insertar_datos_explorador() 
-            #tabla_explorador_asc() 
-            #insertar_datos_explorador_asc()
-
-            # llamar a manejar_insertar
-            #insertar_datos_1()
-            #insertar_datos()
-            #insertar_datos_n()
-            #codigo_html_nivel_1(folder_path)
-            #codigo_html_niveles(folder_path)
-            #codigo_html_nivel_n(folder_path)
-
-            # llamar a manejar_vista
-            #borrar_vista()
-            #crear_vista()
-
-            # llamar a crear_tabla
-            #tabla()
-
+            # obtener los nombres de los arhivos
+            act_i(folder_path)
+            act_ii()
 
             print("datos exportados a BD")        
         else:
             print("Ruta no verificable")
 
 # obtener los nombres de los arhivos
-def obtener_archivos(folder_path):
-    # exportar datos a la BD
-        conn = sqlite3.connect("static/arbol_oficios.db")
-        cursor = conn.cursor()
-        conteo = 0
+def act_i(folder_path):
+    conteo = 0
 
-    
-        # borrar los datos de la BD
-        cursor.execute("DELETE FROM carpetas;")
-        cursor.execute("DELETE FROM archivos;")
-        conn.commit()
+    # borrar los datos de la BD
+    cursor.execute("DELETE FROM carpetas;")
+    cursor.execute("DELETE FROM archivos;")
+    conn.commit()
 
-        # Obtener el largo del texto ingresado
-        largo_ruta = len(folder_path) + 1
+    # Obtener el largo del texto ingresado
+    largo_ruta = len(folder_path) + 1
 
-        # Verificar el tipo de elemento, si es que se ingresa una ruta valida
-        if os.path.exists(folder_path):
-            elements = os.listdir(folder_path)
-            for element in elements:
-                element_path = os.path.join(folder_path, element)  # Ruta completa del elemento
+    # Verificar el tipo de elemento, si es que se ingresa una ruta valida
+    if os.path.exists(folder_path):
+        elements = os.listdir(folder_path)
+        for element in elements:
+            element_path = os.path.join(folder_path, element)  # Ruta completa del elemento
 
-                # extraer el nombre de las carpetas
-                if os.path.isdir(element_path):
-                    # Contar las carpetas
-                    conteo += 1
-                    # Exportar a la base de datos
-                    cursor.execute("INSERT INTO carpetas(carpeta) VALUES(" + "'" + element_path + "'" + ");")
+            # extraer el nombre de las carpetas
+            if os.path.isdir(element_path):
+                # Contar las carpetas
+                conteo += 1
+                # Exportar a la base de datos
+                cursor.execute("INSERT INTO carpetas(carpeta) VALUES(" + "'" + element_path + "'" + ");")
 
-                #  extraer el nombre de los archivos
-                if os.path.isfile(element_path):
-                    cursor.execute("INSERT INTO archivos(archivo) VALUES(" + "'" + element_path[largo_ruta:] + "'" + ");")
-                conn.commit()
-            
-            # segundo nivel
-            while conteo > 0:
-                conteo = 0
-                cursor.execute("SELECT carpeta FROM carpetas;")
-                resultado = cursor.fetchall()
+            #  extraer el nombre de los archivos
+            if os.path.isfile(element_path):
+                cursor.execute("INSERT INTO archivos(archivo) VALUES(" + "'" + element_path[largo_ruta:] + "'" + ");")
+            conn.commit()
+        
+        # segundo nivel
+        while conteo > 0:
+            conteo = 0
+            cursor.execute("SELECT carpeta FROM carpetas;")
+            resultado = cursor.fetchall()
 
-                # Vaciar la tabla de carpetas
-                cursor.execute("DELETE FROM carpetas;")
-                conn.commit()
+            # Vaciar la tabla de carpetas
+            cursor.execute("DELETE FROM carpetas;")
+            conn.commit()
 
-                for res in resultado:
-                    ruta_carpeta = res[0]
-                    
-                    elementos = os.listdir(ruta_carpeta)
-                    for elemento in elementos:
-                        elemento_path = os.path.join(ruta_carpeta, elemento)
-                        # print(elemento_path)
+            for res in resultado:
+                ruta_carpeta = res[0]
+                
+                elementos = os.listdir(ruta_carpeta)
+                for elemento in elementos:
+                    elemento_path = os.path.join(ruta_carpeta, elemento)
+                    # print(elemento_path)
 
-                        #  extraer el nombre de los archivos
-                        if os.path.isfile(elemento_path):
-                            cursor.execute("INSERT INTO archivos(archivo) VALUES(" + "'" + elemento_path[largo_ruta:]  + "'" + ");")
+                    #  extraer el nombre de los archivos
+                    if os.path.isfile(elemento_path):
+                        cursor.execute("INSERT INTO archivos(archivo) VALUES(" + "'" + elemento_path[largo_ruta:]  + "'" + ");")
 
-                        # extraer el nombre de las carpetas
-                        if os.path.isdir(elemento_path):
-                            # Contar las carpetas
-                            conteo += 1
-                            # Exportar a la base de datos
-                            cursor.execute("INSERT INTO carpetas(carpeta) VALUES(" + "'" + elemento_path  + "'" + ");")
-                conn.commit()
+                    # extraer el nombre de las carpetas
+                    if os.path.isdir(elemento_path):
+                        # Contar las carpetas
+                        conteo += 1
+                        # Exportar a la base de datos
+                        cursor.execute("INSERT INTO carpetas(carpeta) VALUES(" + "'" + elemento_path  + "'" + ");")
+            conn.commit()
 
-                if conteo == 0:
-                    break
+            if conteo == 0:
+                break
+
+def act_ii():
+    num_carpetas = []
+    try:
+        cursor.execute("DROP TABLE archivos_i;")
+    except:
+        print("tabla no encontrada")
+
+    # leer cada archivo en la BD
+    cursor.execute("SELECT archivo FROM archivos;")
+    resultado = cursor.fetchall()
+
+    for res in resultado:
+        ruta_archivo = res[0]
+        largo = len(ruta_archivo)
+        cuenta = 0
+
+    # encontrar los separadores de carpeta
+        for letra in range(0,largo):
+            encontrar = ruta_archivo[letra].find("\\")
+            if encontrar > -1:
+                cuenta += 1
+        num_carpetas.append(cuenta)
+    max_num_carpetas = max(num_carpetas)
+
+    # crear la tabla archivos_i
+    texto = ""
+    for col in range(1, max_num_carpetas + 1):
+        agregar = "nivel_" + str(col) + " TEXT, " + "ruta_nivel_" + str(col) + " TEXT, "
+        texto = texto + agregar
+    texto_f = texto[0:len(texto)-2]
+
+    sentencia_sql = "CREATE TABLE archivos_i(ruta_archivo TEXT, " + texto_f + ");"
+    cursor.execute(sentencia_sql)
+    conn.commit()
+
+    conn.close()
+
+
 
 # datos del formulario de tkinter  ------------------------------------------------------------------------
 root = Tk()
